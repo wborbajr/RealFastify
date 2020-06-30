0e958040d5512e31f828e372dd4ebddeb93af420
Waldir Borba Junior <wborbajr@gmail.com>
2020-06-30 13:58:35 -0300

    feat: README fixed

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# RealFastify
-Proof of Concept using Fastify
+# RealFastify REST API, made with Node.js, MongoDB, Fastify and Swagger.
+
+> Proof of Concept using Fastify
+>
+> Basically, an example Node.js project to manage visitors of a hypothetical center.
+>
+> Just for practical and educational purposes.
+
+## Prerequisites
+
+- [Nodejs](https://nodejs.org)
+- MongoDB, [locally installed](https://www.mongodb.com/download-center/community) or provided by a [docker container](https://hub.docker.com/_/mongo)
+
+## Build Setup
+
+```bash
+# (optional) create a docker container for the data base
+docker run --name dev-mongo -d mongo:latest
+
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:3000
+npm start
+```
+
+## License
+
+[MIT License](https://github.com/wborbajr/RealFastify/blob/master/LICENSE)
--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ const fastify = require("fastify")({
   logger: true,
 });
 
-const dotenv = require("dotenv");
 const cors = require("fastify-cors");
+const dotenv = require("dotenv");
 const dbconnect = require("./config/mongo.config");
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({
-    path: "/config/ecosystem.dev.env",
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+dotenv.config({
+  path: "./config/ecosystem.dev.env",
+});
+// }
 
-// Import Routes
+// import routes
 const routes = require("./routes");
 
 // Import Swagger Options
@@ -21,14 +21,16 @@ const swagger = require("./config/swagger");
 
 // Registering plugins to fastify
 fastify.register(dbconnect);
+
+// enable cors
 fastify.register(cors, {
   origin: "*",
 });
 
-const PORT = 5000; //process.env.PORT || 5050;
-const ADDRESS = "127.0.0.1"; // process.env.ADDRESS || "127.0.0.1";
+const PORT = process.env.PORT || "5000";
+const ADDRESS = process.env.ADDRESS || "127.0.0.1";
 
-// Loop over each route
+// register routes
 routes.forEach((route, index) => {
   fastify.route(route);
 });
@@ -36,8 +38,8 @@ routes.forEach((route, index) => {
 const start = async () => {
   try {
     await fastify.ready();
-    await fastify.listen(PORT, ADDRESS);
-    const log = `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT} .:: on http://${ADDRESS}:${PORT} ::. Main:(${__filename})`;
+    const addr = await fastify.listen(PORT, ADDRESS);
+    const log = `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT} .:: on http://${addr}:${PORT} ::. Main:(${__filename})`;
 
     fastify.log.info(log);
   } catch (error) {
